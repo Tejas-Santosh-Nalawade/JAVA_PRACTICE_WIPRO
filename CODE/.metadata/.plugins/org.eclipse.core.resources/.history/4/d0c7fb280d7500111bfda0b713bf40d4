5c9c0386dfcf5b41373fddd1077b625b51aa30d7
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2026-07-02 10:45:42 +0530

    Day 2 Java Basics and Array

---
+++ CODE/.metadata/.plugins/org.eclipse.core.resources/.history/4/d0c7fb280d7500111bfda0b713bf40d4
@@ -0,0 +1,7 @@
+package Hello;
+
+public class Mathoperations{ 
+	public static void main(String [] args) {
+		System.out.println(2 + 5);
+	}
+}
--- CODE/Basics/src/Hello/HelloWorld.java
+++ CODE/Basics/src/Hello/HelloWorld.java
@@ -3,6 +3,11 @@ package Hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		int rollno; // Instance Variable
+		void display() {
+			int num; // Local Variable 
+		}
+		
 		System.out.println("Hello World");
 	}
 
